5d399943544490ea617de58e898645ab5ec091ee
vasilevae.24 <av9810636@gmail.com>
2022-12-24 22:32:32 +0300

    Recharge.Time.0.0

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        GameObject gameObject=new GameObject();
+        GameObject gameObject=new GameObject();  // вообщем то бесполезно
         Tank tank1=new Tank(750,300, 1,  0.05, 900, 20, 5);
         Tank tank2=new Tank(100, 100, 1, 0.05, 900, 1000, 7);
         Wall wall=new Wall(700, 400, 200, 25);
